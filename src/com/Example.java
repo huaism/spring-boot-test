95e4f3375a788c29696dc2aafce4808e8cf4ca24
marvin <marvinhuai@joytrav.com>
2017-02-07 14:19:47 +0800

    test

--- src/com/Example.java
+++ src/com/Example.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class Example {
 	 	@RequestMapping("/")
 	    String home() {
-	        return "Hello World!";
+	        return "Hello World---!----";
 	    }
 
 	    public static void main(String[] args) throws Exception {
